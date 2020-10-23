3864fc5b53963d9c37ecb3e36da75e1cd01688ad
Niek van der Maas <mail@niekvandermaas.nl>
2020-10-23 17:47:13 +0200

    Use if statement

--- update.sh
+++ update.sh
@@ -15,25 +15,25 @@ echo "Chrome stable: ${CHROME_STABLE}, unstable: ${CHROME_UNSTABLE}"
 echo "Firefox stable: ${FIREFOX_STABLE}, unstable: ${FIREFOX_UNSTABLE}"
 
 # Update stable versions in JSON
-[[ -z $(jq '.chrome.stable[] | select (.version=="'$CHROME_STABLE'")' browsers.json) ]] \
-  && echo "Stable Chrome version not found yet in JSON" \
-  && jq '.chrome.stable += [{"version": "'$CHROME_STABLE'", "updated": "'$NOW'"}]' browsers.json > browsers.tmp \
-  && mv browsers.tmp browsers.json \
-  && echo "Updated stable Chrome version to ${CHROME_STABLE}"
-[[ -z $(jq '.firefox.stable[] | select (.version=="'$FIREFOX_STABLE'")' browsers.json) ]] \
-  && echo "Stable Firefox version not found yet in JSON" \
-  && jq '.firefox.stable += [{"version": "'$FIREFOX_STABLE'", "updated": "'$NOW'"}]' browsers.json > browsers.tmp \
-  && mv browsers.tmp browsers.json \
-  && echo "Updated stable Firefox version to ${FIREFOX_STABLE}"
+if [[ -z $(jq '.chrome.stable[] | select (.version=="'${CHROME_STABLE}'")' browsers.json) ]]; then
+  jq '.chrome.stable += [{"version": "'${CHROME_STABLE}'", "updated": "'${NOW}'"}]' browsers.json > browsers.tmp
+  mv browsers.tmp browsers.json
+  echo "Updated stable Chrome version to ${CHROME_STABLE}"
+fi
+if [[ -z $(jq '.firefox.stable[] | select (.version=="'${FIREFOX_STABLE}'")' browsers.json) ]]; then
+  jq '.firefox.stable += [{"version": "'${FIREFOX_STABLE}'", "updated": "'${NOW}'"}]' browsers.json > browsers.tmp
+  mv browsers.tmp browsers.json
+  echo "Updated stable Firefox version to ${FIREFOX_STABLE}"
+fi
 
 # Update unstable versions in JSON
-[[ -z $(jq '.chrome.unstable[] | select (.version=="'$CHROME_UNSTABLE'")' browsers.json) ]] \
-  && echo "Unstable Chrome version not found yet in JSON" \
-  && jq '.chrome.unstable += [{"version": "'$CHROME_UNSTABLE'", "updated": "'$NOW'"}]' browsers.json > browsers.tmp \
-  && mv browsers.tmp browsers.json \
-  && echo "Updated unstable Chrome version to ${CHROME_UNSTABLE}"
-[[ -z $(jq '.firefox.unstable[] | select (.version=="'$FIREFOX_UNSTABLE'")' browsers.json) ]] \
-  && echo "Unstable Firefox version not found yet in JSON" \
-  && jq '.firefox.unstable += [{"version": "'$FIREFOX_UNSTABLE'", "updated": "'$NOW'"}]' browsers.json > browsers.tmp \
-  && mv browsers.tmp browsers.json \
-  && echo "Updated unstable Firefox version to ${FIREFOX_UNSTABLE}"
+if [[ -z $(jq '.chrome.unstable[] | select (.version=="'${CHROME_UNSTABLE}'")' browsers.json) ]]; then
+  jq '.chrome.unstable += [{"version": "'${CHROME_UNSTABLE}'", "updated": "'${NOW}'"}]' browsers.json > browsers.tmp
+  mv browsers.tmp browsers.json
+  echo "Updated unstable Chrome version to ${CHROME_UNSTABLE}"
+fi
+if [[ -z $(jq '.firefox.unstable[] | select (.version=="'${FIREFOX_UNSTABLE}'")' browsers.json) ]]; then
+  jq '.firefox.unstable += [{"version": "'${FIREFOX_UNSTABLE}'", "updated": "'${NOW}'"}]' browsers.json > browsers.tmp
+  mv browsers.tmp browsers.json
+  echo "Updated unstable Firefox version to ${FIREFOX_UNSTABLE}"
+fi
